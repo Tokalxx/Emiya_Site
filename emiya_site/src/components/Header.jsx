249fed838e44196b642a27ad5363496895e4c668
Sean Maseola <SeanSMaseola@gmail.com>
2025-02-07 06:54:22 +0200

    Add css design to the html elements

--- emiya_site/src/components/Header.jsx
+++ emiya_site/src/components/Header.jsx
@@ -1,23 +1,26 @@
 import React, { Component } from "react";
+import logo from "../assets/Archer_Logo.png";
+import "./Components.css";
 
 export class Header extends Component {
   render() {
-    return;
-    <div>
-      <div className="Nav_Wrapper">
-        <div className="Nav_Logo">
-          <img src="" className="Nav_Logo" />
-          <h1 className="Nav_header">Emiya Site</h1>
-        </div>
-        <div className="Nav_Links">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Details</li>
-          </ul>
+    return (
+      <div>
+        <div className="Nav_Wrapper">
+          <div className="Nav_Logo">
+            <img src={logo} className="Nav_Image" />
+            <h1 className="Nav_header">Emiya Site</h1>
+          </div>
+          <div className="Nav_Links">
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Details</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>;
+    );
   }
 }
 
